f85b06416f04d7c9bc8c4836bc6ecce6ba7a2ce8
Dondi Michael Stroma <dstroma@gmail.com>
2021-07-20 18:38:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+List::Utils::MoveElement - It's new $module
+
+# SYNOPSIS
+
+    use List::Utils::MoveElement;
+
+# DESCRIPTION
+
+List::Utils::MoveElement is ...
+
+# LICENSE
+
+Copyright (C) Dondi Michael Stroma.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Dondi Michael Stroma <dstroma@gmail.com>
---
+++ lib/List/Utils/MoveElement.pm
@@ -0,0 +1,39 @@
+package List::Utils::MoveElement;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+List::Utils::MoveElement - It's new $module
+
+=head1 SYNOPSIS
+
+    use List::Utils::MoveElement;
+
+=head1 DESCRIPTION
+
+List::Utils::MoveElement is ...
+
+=head1 LICENSE
+
+Copyright (C) Dondi Michael Stroma.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Dondi Michael Stroma E<lt>dstroma@gmail.comE<gt>
+
+=cut
+
